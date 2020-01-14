20f76b0c2b1e5cc0e0f5f5134fd34f867c75d028
Chanjong Baek <readandaction@gmail.com>
2020-01-15 07:09:26 +0900

    2.16

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middleWares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
+
+app.use(helmet());
 app.set("view engine", "pug");
 // app.use(betweenHome);
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
 // "/"는 홈을 의미
 // middleware 사용법 중 첫번째
 //app.get("/", betweenHome, handleHome);
-
+app.use(localsMiddleware);
 // .use는 사용자가 지정된 경로에 들어갔을 시 그 뒤 함수를 실행하겠다는 명령어.
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
