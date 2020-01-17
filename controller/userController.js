4dcdba1f5020913000f938fa88caa4e7e3ee6c3d
Chanjong Baek <readandaction@gmail.com>
2020-01-17 09:38:35 +0900

    #2.25 Plus Video Detail

--- controller/userController.js
+++ controller/userController.js
@@ -5,7 +5,10 @@ export const getLogin = (req, res) =>
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+  // To Do : Process Log Out
+  res.redirect(routes.home);
+};
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = (req, res) => {
