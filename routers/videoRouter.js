5b255b6d34730000d2b43f480349c3a76474c287
Chanjong Baek <readandaction@gmail.com>
2020-01-24 16:00:03 +0900

    #3.10 deletVideo

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -18,7 +18,7 @@ videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 // export defalut는 파일로 export 한다는 뜻
 export default videoRouter;
