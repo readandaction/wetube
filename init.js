4f78586f7f0630256c43f31599bcf89389ae5247
Chanjong Baek <readandaction@gmail.com>
2020-01-30 16:34:16 +0900

    #6.2

--- init.js
+++ init.js
@@ -1,11 +1,13 @@
 import "./db";
 
-import app from "./app";
 import dotenv from "dotenv";
-import "./models/Video";
-import "./models/Comment";
+import app from "./app";
 
 dotenv.config();
+
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 const handleListening = () =>
