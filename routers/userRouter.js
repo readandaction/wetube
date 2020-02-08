3567fa04d4e0df347598de65af82ab63ef953ed5
Chanjong Baek <readandaction@gmail.com>
2020-02-08 17:35:00 +0900

    #7.1 error profile upload X

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,14 +3,16 @@ import routes from "../routes";
 import {
   users,
   userDetail,
-  editProfile,
-  changePassword
+  getEditProfile,
+  changePassword,
+  postEditProfile
 } from "../controller/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 userRouter.get(routes.users, users);
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
