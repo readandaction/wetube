bf04d39538f37a4d05cdc64884395f12dd8809fc
Chanjong Baek <readandaction@gmail.com>
2020-02-10 09:23:12 +0900

    #7.3 error there is no push

--- controller/videoController.js
+++ controller/videoController.js
@@ -34,8 +34,11 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
